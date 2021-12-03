cca63ad200536277349423e902b1b409ba36436e
souza1806 <22303759+souza1806@users.noreply.github.com>
2021-12-02 22:21:44 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialExampleModule} from '../material.module';
+import {TableColumnStylingExample} from './table-column-styling-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [TableColumnStylingExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [TableColumnStylingExample],
+})
+export class AppModule {}
