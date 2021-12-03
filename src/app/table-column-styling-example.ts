83a80164c12d1bdc3dab20ac947f336e649946ee
souza1806 <22303759+souza1806@users.noreply.github.com>
2021-12-02 22:38:35 -0300

    Update table-column-styling-example.ts

--- src/app/table-column-styling-example.ts
+++ src/app/table-column-styling-example.ts
@@ -3,21 +3,21 @@ import {Component} from '@angular/core';
 export interface PeriodicElement {
   name: string;
   position: number;
-  weight: number;
+  weight: string;
   symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Brutus Militsa Jabin', weight: '19/11/2004', symbol: '17/02/2005'},
+  {position: 2, name: 'Ruaidhrí Naz Ivona', weight: '31/01/2007', symbol: '01/05/2007'},
+  {position: 3, name: 'Dimitar Lída Pollux', weight: '23/04/2008', symbol: '22/07/2008'},
+  {position: 4, name: 'Stanko Raziel Marguerite', weight: '30/08/2012', symbol: '28/11/2012'},
+  {position: 5, name: 'Mihael Kassiopeia Rosaura', weight: '02/09/2014', symbol: '01/12/2014'},
+  {position: 6, name: 'Funmilayo Zlata Dragoslav', weight: '28/10/2014', symbol: '26/01/2015'},
+  {position: 7, name: 'Teodor Swaran Nelly', weight: '20/03/2017', symbol: '18/06/2017'},
+  {position: 8, name: 'Casimir Berniece Richard', weight: '05/06/2017', symbol: '03/09/2017'},
+  {position: 9, name: 'Maarika Anatol Carry', weight: '26/10/2018', symbol: '24/01/2019'},
+  {position: 10, name: 'Vita Janet Roland', weight: '06/03/2020', symbol: '04/06/2020'},
 ];
 
 /**
